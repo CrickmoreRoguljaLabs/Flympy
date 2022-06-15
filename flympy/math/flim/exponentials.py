396f20e47675a4b1e9cc8be782381ceebc87ca87
Stephen <thornquist@fas.harvard.edu>
2022-06-15 19:13:23 -0400

    FLIM Fitting in progress

--- flympy/math/flim/exponentials.py
+++ flympy/math/flim/exponentials.py
@@ -25,10 +25,9 @@ SCT March 27 2021
 import numpy as np
 from scipy import special
 
-from .flimparams import FLIMParams, Exp, Irf
-
-def px_chi_sq_exp(photon_arrivals : np.ndarray, flimparams : FLIMParams)->float:
-    """ Returns the chi-squared statistic
+def chi_sq_exp(photon_arrivals : np.ndarray, param_tuple : tuple, cut_negatives : bool = True)->float:
+    """
+    Returns the chi-squared statistic
     of a photon arrival data set, given
     parameters in params
 
@@ -38,9 +37,9 @@ def px_chi_sq_exp(photon_arrivals : np.ndarray, flimparams : FLIMParams)->float:
 
         Histogrammed arrival time of each photon. Element n is the count of photons arriving in histogram bin n.
 
-    flimparams : FLIMParams
+    param_tuple : tuple
         
-        A FLIMParams file with fit parameters.
+        A tuple consistent with the FLIMParams property "param_tuple"
     
     RETURN VALUES
     ----------
@@ -49,16 +48,19 @@ def px_chi_sq_exp(photon_arrivals : np.ndarray, flimparams : FLIMParams)->float:
         Chi-squared statistic of the histogram data under the model of the FLIMParams object
     """
     arrival_p = np.zeros(photon_arrivals.shape) # default, will be overwritten
-    # params needed for all exponential models
-    t_o = flimparams.irf.t_offset 
-    tau_g = flimparams.irf.sigma
-    num_exps = flimparams.ncomponents
 
-    # iterate through components, adding up likelihood values
-    for component in flimparams.exps:
-        # get params for this component of the distribution
+    t_o = param_tuple[-2]
+    tau_g = param_tuple[1]
 
-        arrival_p += component.frac * monoexponential_prob(np.arange(arrival_p.shape[0])-t_o,component.tau, tau_g)
+    #iterate through components, adding up likelihood values
+    n_exps = len(param_tuple-2)//2
+    for exp_idx in range(n_exps):
+        arrival_p += param_tuple[2*exp_idx+1] * monoexponential_prob(
+            np.arange(arrival_p.shape[0])-t_o, # x_range
+            param_tuple[2*exp_idx], #tau
+            tau_g,
+            cut_negatives=cut_negatives
+        )
 
     total_photons = np.sum(photon_arrivals)
 
@@ -68,60 +70,40 @@ def px_chi_sq_exp(photon_arrivals : np.ndarray, flimparams : FLIMParams)->float:
 
     return np.nansum(chi_sq)
 
-
-def px_log_likelihood_exp(photon_arrivals, params):
-    """ Returns the log-likelihood of a single pixel's distribution of arrival times under the presumption of the input params
-
-    INPUTS
-    ----------
-    photon_arrival (1-dim ndarray):
-        Histogrammed arrival time of each photon. Element n is the count of photons arriving in histogram bin n.
-
-    params(dict). A params file like the others. Perhaps I'll make this a class
-    
-    RETURN VALUES
-    ----------
-    LL (double) -- log-likelihood of the photon
-    """
-    arrival_p = np.zeros(photon_arrivals.shape) # default, will be overwritten
-    # params needed for all models
-    t_o = params['T_O']
-    tau_g = params['IRF']['PARAMS']['SIGMA']
-    num_exps = params['NCOMPONENTS']
-
-    # iterate through components, adding up likelihood values
-    for component in range(num_exps):
-        # get params for this component of the distribution
-        comp_dict = params['EXPPARAMS'][component]
-        tau_c = comp_dict['TAU']
-        f = comp_dict['FRAC']
-
-        arrival_p += f * monoexponential_prob(np.arange(arrival_p.shape[0])-t_o,tau_c, tau_g)
-
-    return np.nansum(np.log(arrival_p)*photon_arrivals)
-
 def monoexponential_prob(x_range : np.ndarray, tau : float, tau_g : float, cut_negatives : bool = True)->float:
-    """ Takes in parameters of an exponential distribution
+    """
+    
+    Takes in parameters of an exponential distribution
     convolved with a Gaussian, and outputs the probability
-    of each element of x_range
+    of each element of x_range.
+
+    Presumes the temporal offset has been subtracted out already!
     TODO: Switch from pointwise estimate to bin-size integral
     
     
     INPUTS
     ----------
 
-    x_range (ndarray) -- The range of values to compute the probability of.
+    x_range : np.ndarray
+        
+        The range of values to compute the probability of.
 
-    tau (double) -- The decay parameter of the exponential
+    tau : float
+    
+        The decay parameter of the exponential (IN UNITS OF BINS OF THE ARRAY)
 
-    tau_g (double) -- Width of the Gaussian with which the exponential
-    distribution has been convolved
+    tau_g : float
+    
+        Width of the Gaussian with which the exponential
+        distribution has been convolved (IN UNITS OF BINS OF THE ARRAY)
 
 
     RETURN VALUES
     ----------
 
-    p (ndarray) -- Probability of each corresponding element of x_range
+    p : np.ndarray
+    
+        Probability of each corresponding element of x_range
     """
     gauss_coeff = (1/(2*tau)) * np.exp((tau_g**2)/(2*(tau**2)))
     normalization = special.erfc( -(tau * x_range - tau_g**2)/ (np.sqrt(2)*tau_g*tau) )
--- flympy/math/flim/flimparams.py
+++ flympy/math/flim/flimparams.py
@@ -1,5 +1,7 @@
 import numpy as np
+from scipy.optimize import minimize, Bounds, LinearConstraint, OptimizeResult
 
+from .exponentials import chi_sq_exp
 from .flimunits import FlimUnits
 
 class FLIMParams():
@@ -8,15 +10,25 @@ class FLIMParams():
     to fitting distributions of fluorescence
     lifetime or photon arrival data.
 
-    Currently only implements mono and diexponentials.
+    Currently only implements combinations of exponentials.
     """
     
-    def __init__(self, *args, color_channel : int = 0, units = FlimUnits.ARRIVAL_BINS, **params):
+    def __init__(self, *args, color_channel : int = None, units = FlimUnits.ARRIVAL_BINS, **params):
         
         self.exps = [arg for arg in args if isinstance(arg, Exp)]
-        self.irf = next(isinstance(x, Irf) for x in args, None)
+        self.irf = next((x for x in args if isinstance(x, Irf)), None)
         self.color_channel = color_channel
         self.units = units
+
+    @property
+    def tau_g(self)->float:
+        if hasattr(self, 'irf'):
+            return self.irf.tau_g
+
+    @property
+    def tau_offset(self)->float:
+        if hasattr(self, 'irf'):
+            return self.irf.tau_offset
 
     @property
     def param_tuple(self)->tuple:
@@ -27,7 +39,6 @@ class FLIMParams():
         retlist = []
         for exp in self.exps:
             retlist += exp.param_list
-
         retlist += self.irf.param_list
         return tuple(retlist)    
 
@@ -55,13 +66,98 @@ class FLIMParams():
         -------
         None
         """
-        pass
+        raise NotImplementedError("Haven't implemented unit conversions in FLIMParams yet.")
 
     @property
-    def ncomponents(self):
+    def ncomponents(self)->int:
+        """ Number of exponentials in this FLIMParams """
         if hasattr(self, 'exps'):
             return len(self.exps)
         return 0
+
+    def fit_to_data(self, data : np.ndarray, num_exps : int = 2, initial_guess : tuple = None, metric : callable = None)->OptimizeResult:
+        """
+        Takes in the data and adjusts the internal
+        parameters of this FLIMParams object to
+        minimize the metric input. Default is CHI-SQUARED.
+
+        Stores new parameter values IN PLACE, but will return
+        the scipy OptimizeResult object
+
+        Inputs
+        ------
+        data : np.ndarray
+
+            A numpy array of the arrival time histogram. Data[n] = number
+            of photons arriving in bin n
+
+        num_exps : int
+
+            Number of differently distributed monoexponential states.
+
+        initial_guess : tuple
+
+            Guess for initial params in FLIMParams.param_tuple format.
+        
+        metric : callable
+
+            Defines the cost function for curve fitting. Defaults to chi-squared
+
+            Argument 1: DATA (1d-ndarray) as above
+
+            Argument 2: PARAMS (tuple)
+
+        Returns
+        -------
+
+        fit : scipy.optimize.OptimizeResult
+
+            The OptimizeResult object for diagnostics on the fit.
+        """
+
+        if metric is None:
+            objective = lambda x: chi_sq_exp(data, x)
+
+        else:
+            objective = lambda x: metric(data, x)
+
+        # Just some numbers. Assumes lifetimes of 1200 picoseconds, 2400 picoseconds etc. ascending.
+        if initial_guess is None:
+            x0 = []
+            for exp in range(num_exps):
+                x0 += [1.0/num_exps, 6*(exp+1)]
+            x0 += [4, 0.2] # tau_offset, tau_g
+            initial_guess = tuple(x0) 
+
+        fit_obj = minimize(objective, initial_guess, method='trust-constr',
+               constraints=generate_linear_constraints_trust(initial_guess),
+               bounds=generate_bounds(initial_guess)
+        )
+
+        fit_tuple = fit_obj.x
+
+        self.exps = [Exp(tau=fit_tuple[2*exp_idx], frac = fit_tuple[2*exp_idx + 1]) for exp_idx in range(num_exps)]
+        self.irf = Irf()
+        
+        self.T_O = fit_tuple[-2]
+        self.IRF = Irf(tau_offset = fit_tuple[-2], tau_g = fit_tuple[-1])
+
+        return fit_obj
+
+    def chi_sq(self, data : np.ndarray, cut_negatives : bool = True)->float:
+        """ Presumes all units are in ARRIVAL_BIN units """
+        return chi_sq_exp(data, self.param_tuple, cut_negatives=cut_negatives)
+
+    def probability_dist(self, data :np.ndarray):
+        raise NotImplementedError()
+
+    def __repr__(self):
+        retstr = "FLIMParams object: \n\n"
+        retstr += "\tParameters:\n"
+        for exp in self.exps:
+            retstr += "\t\t"+exp.__repr__() + "\n"
+        retstr += "\t\t"+self.irf.__repr__() + "\n"
+        return retstr
 
 class FLIMParameter():
     """
@@ -89,11 +185,73 @@ class FLIMParameter():
     def param_tuple(self)->tuple:
         return tuple(self.param_list)
 
+    def __repr__(self):
+        retstr = self.__class__.__name__ + "\n"
+        for param in self.__class__.class_params:
+            retstr += "\t" + str(param) + " : " + str(getattr(self,param)) + "\n"
+        return retstr
+
 class Exp(FLIMParameter):
     """ Monoexponential parameter fits """
     class_params = ['tau', 'frac']
+    def __init__(self, **params):
+        """ Exp(tau : float, frac : float) """
+        super().__init__(**params)
+
 
 class Irf(FLIMParameter):
     """ Instrument response function """
-    class_params = ['tau_g', 'tau_offset']
+    class_params = ['tau_offset', 'tau_g']
+    def __init__(self, **params):
+        """ Irf(tau_offset : float, tau_g : float)"""
+        super().__init__(**params)
 
+
+### LOCAL FUNCTIONS
+
+
+def generate_bounds(param_tuple : tuple)->Bounds:
+    """
+    All params > 0
+    
+    fracs < 1
+
+    noise < 1
+    
+    Param order:
+    tau_1
+    frac_1
+    ...
+    tau_n
+    frac_n
+    t_offset
+    tau_g
+    """
+    n_exps = (len(param_tuple)-2)//2
+    lower_bounds_frac = [0 for x in range(n_exps)]
+    lower_bounds_tau = [0 for x in range(n_exps)]
+    
+    lb = [val for pair in zip(lower_bounds_tau, lower_bounds_frac) for val in pair]
+    lb.append(0.0) # tau_o
+    lb.append(0.0) # tau_g
+    
+    upper_bounds_frac = [1 for x in range(n_exps)]
+    upper_bounds_tau = [np.inf for x in range(n_exps)]
+    
+    ub = [val for pair in zip(upper_bounds_tau, upper_bounds_frac) for val in pair]
+    ub.append(np.inf) # tau_o
+    ub.append(np.inf) # tau_g
+
+    return Bounds(lb, ub)
+
+def generate_linear_constraints_trust(param_tuple : tuple)->LinearConstraint:
+    """ Only one linear constraint, sum of fracs == 1"""
+
+    lin_op = np.zeros_like(param_tuple)
+    n_exps = (len(param_tuple)-2)//2
+    # Tuple looks like:
+    # (tau, frac, tau, frac, ... , tau_o, tau_g)
+    for exp_idx in range(n_exps):
+        lin_op[(exp_idx * 2) + 1] = 1
+    
+    return LinearConstraint(lin_op,1.0,1.0)
